59ede7046da25938292dd293b2fe666f35dfdadb
tlaudahl <travis.laudahl@gmail.com>
2019-09-20 10:36:58 -0500

    Data is fetched and being displayed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p># of Episodes: {props.episode}</p>
+      <p>Species: {props.species}</p>
+      <p>Location: {props.location.name}</p>
+    </div>
+  )
 }
